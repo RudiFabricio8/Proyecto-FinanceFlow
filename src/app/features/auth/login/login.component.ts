ca82b87f768987353e8392b45068b4a513fdc6e8
grafica <fabriciojaimes775@gmail.com>
2025-12-01 20:15:45 -0600

    login completed

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,5 +1,60 @@
-export class LoginComponent {
-  username = '';
-  password = '';
-  submit() { /* TODO: integrate with AuthService */ }
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  submitting = false;
+  errorMsg = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls as { [key: string]: AbstractControl };
+  }
+
+  onSubmit(): void {
+    this.errorMsg = '';
+
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.submitting = true;
+    const { email, password } = this.form.value;
+
+    this.auth.login(email!, password!).subscribe({
+      next: (res: { token: string }) => {
+        this.auth.setToken(res.token);
+        this.submitting = false;
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err: Error) => {
+        this.submitting = false;
+        this.errorMsg = err?.message || 'Error al iniciar sesión';
+      }
+    });
+  }
 }
